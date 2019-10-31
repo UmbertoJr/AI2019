9687b675a77f6657a6f5eff56ad53581a32c2fe9
UmbertoJr <u.mele.coding@gmail.com>
2019-10-31 19:09:18 +0100

    udpate

--- src/__init__.py
+++ src/__init__.py
@@ -4,5 +4,5 @@ if 'AI' in os.getcwd():
     from src.TSP_solver import *
     from src.io_tsp import *
 else:
-    from AI2019.code.TSP_solver import *
-    from AI2019.code.io_tsp import *
+    from AI2019.src.TSP_solver import *
+    from AI2019.src.io_tsp import *
