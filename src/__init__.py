68e55698ca6353def2d3dc6b561b7bf290e02333
UmbertoJr <u.mele.coding@gmail.com>
2019-11-09 16:42:23 +0100

    config

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,10 @@
 import os
 
 if 'AI' in os.getcwd():
+    from src.utils import *
     from src.TSP_solver import *
     from src.io_tsp import *
-    from src.utils import *
 else:
+    from AI2019src.utils import *
     from AI2019.src.TSP_solver import *
     from AI2019.src.io_tsp import *
-    from AI2019src.utils import *
