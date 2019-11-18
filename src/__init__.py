ed270740c5495a5aa49ca94b6caa35cc3fed4707
UmbertoJr <u.mele.coding@gmail.com>
2019-11-18 08:51:42 +0100

    init

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,7 @@ if 'AI' in os.getcwd():
     from src.utils import *
     from src.constructive_algorithms import *
     from src.local_search import *
+    from src.meta_heuristics import *
     from src.TSP_solver import *
     from src.io_tsp import *
 
@@ -11,5 +12,6 @@ else:
     from AI2019.src.utils import *
     from AI2019.src.constructive_algorithms import *
     from AI2019.src.local_search import *
+    from AI2019.src.meta_heuristics import *
     from AI2019.src.TSP_solver import *
     from AI2019.src.io_tsp import *
---
+++ src/meta_heuristics.py
@@ -0,0 +1,10 @@
+class Simulated_Annealing:
+
+    def __call__(self):
+        pass
+
+
+class Iterated_Local_Search:
+
+    def __call__(self):
+        pass
